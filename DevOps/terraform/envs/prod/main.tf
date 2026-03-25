6828801b5d06d5cdfc22de4ba226d8e180bcad51
vetaorigin <vetaorigin@gmail.com>
2026-03-25 12:23:36 +0100

    made few changes in dbs infra

--- DevOps/terraform/envs/prod/main.tf
+++ DevOps/terraform/envs/prod/main.tf
@@ -1,20 +1,21 @@
 module "rds" {
   source = "../../modules/rds"
 
-  private_subnets        = var.private_subnets
-  rds_username           = var.rds_username
-  rds_password           = var.rds_password
-  rds_db_name            = var.rds_db_name
-  rds_instance_class     = var.rds_instance_class
-  rds_allocated_storage  = var.rds_allocated_storage
+  private_subnets       = var.private_subnets
+  rds_username          = var.rds_username
+  rds_password          = var.rds_password
+  rds_db_name           = var.rds_db_name
+  rds_instance_class    = var.rds_instance_class
+  rds_allocated_storage = var.rds_allocated_storage
 }
 
 module "redis" {
   source = "../../modules/elasticache"
 
-  redis_subnets          = var.redis_subnets
-  redis_node_type        = var.redis_node_type
-  redis_num_cache_nodes  = var.redis_num_cache_nodes
-  redis_engine_version   = var.redis_engine_version
-  redis_password         = var.redis_password
+  redis_subnets           = var.private_subnets
+  redis_subnet_group_name = "veta-redis-subnet-group"
+  redis_node_type         = var.redis_node_type
+  redis_num_cache_nodes   = var.redis_num_cache_nodes
+  redis_engine_version    = var.redis_engine_version
+  redis_password          = var.redis_password
 }
--- DevOps/terraform/envs/prod/variables.tf
+++ DevOps/terraform/envs/prod/variables.tf
@@ -1,15 +1,62 @@
-variable "private_subnets" { type = list(string) }
-variable "redis_subnets" { type = list(string) }
+# Subnets
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet IDs"
+}
 
 # RDS
-variable "rds_username" {}
-variable "rds_password" {}
-variable "rds_db_name" {}
-variable "rds_instance_class" {}
-variable "rds_allocated_storage" {}
+variable "rds_username" {
+  type        = string
+  description = "RDS master username"
+}
+
+variable "rds_password" {
+  type        = string
+  description = "RDS master password"
+}
+
+variable "rds_db_name" {
+  type        = string
+  description = "RDS database name"
+}
+
+variable "rds_instance_class" {
+  type        = string
+  description = "RDS instance type"
+}
+
+variable "rds_allocated_storage" {
+  type        = number
+  description = "RDS allocated storage in GB"
+}
 
 # Redis
-variable "redis_node_type" {}
-variable "redis_num_cache_nodes" {}
-variable "redis_engine_version" {}
-variable "redis_password" {}
+variable "redis_subnets" {
+  type        = list(string)
+  description = "List of subnets for Redis"
+}
+
+variable "redis_subnet_group_name" {
+  type        = string
+  description = "Name of Redis subnet group"
+}
+
+variable "redis_node_type" {
+  type        = string
+  description = "Redis node type (e.g., cache.t3.micro)"
+}
+
+variable "redis_num_cache_nodes" {
+  type        = number
+  description = "Number of Redis cache nodes"
+}
+
+variable "redis_engine_version" {
+  type        = string
+  description = "Redis engine version"
+}
+
+variable "redis_password" {
+  type        = string
+  description = "Redis password"
+}
